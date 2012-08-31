e6accd71529ae438dde6bc5bfcace0cb8e2cc233
Thom Neale <twneale@gmail.com>
2012-08-31 14:21:47 -0400

    experiment with integrating turkeyvulture into public site

--- experimental/turkeyvulture/build_index.py
+++ experimental/turkeyvulture/build_index.py
@@ -10,15 +10,16 @@ templates = {
     'legislators': Template(
         '{{obj.full_name}} {{obj.district}} {{obj.party}}'),
 
-    'bills': Template('''
-        {{obj.bill_id}} {{obj.title}}
-        {% for subject in obj.subjects %}
-            {{ subject }}
-        {% endfor %}
-        ''')
+    # 'bills': Template('''
+    #     {{obj.bill_id}} {{obj.title}}
+    #     {% for subject in obj.subjects %}
+    #         {{ subject }}
+    #     {% endfor %}
+    #     ''')
     }
 
-if __name__ == '__main__':
+
+def build_index(state):
     from billy.models import db
     index = jsindex.IndexBuilder()
 
@@ -26,7 +27,7 @@ if __name__ == '__main__':
     storekeys = ['full_name', '_type', 'chamber', 'district', 'party',
                  'state', '_id', 'photo_url']
     coll = getattr(db, cname)
-    spec = {'state': 'ca', 'active': True}
+    spec = {'state': state, 'active': True}
     objects = coll.find(spec)
     print 'adding', objects.count(), cname, 'with spec %r' % spec
     renderer = lambda obj: templates[cname].render(obj=obj)
@@ -35,21 +36,26 @@ if __name__ == '__main__':
     cname = 'committees'
     storekeys = ['committee', 'chamber', '_type', 'state', '_id', 'members']
     coll = getattr(db, cname)
-    spec = {'state': 'ca'}
+    spec = {'state': state}
     objects = coll.find(spec)
     print 'adding', objects.count(), cname, 'with spec %r' % spec
     renderer = lambda obj: templates[cname].render(obj=obj)
     index.add(cname[0], objects, renderer, all_substrs=True, storekeys=storekeys)
 
-    spec.update(session='20112012')
-    storekeys = ['bill_id', 'title', '_type', 'subjects', 'type', 'scraped_subjects',
-                 'state', '_id', 'session']
-    objects = db.bills.find(spec)
-    print 'adding', objects.count(), 'bills', 'with spec %r' % spec
-    renderer = lambda obj: templates['bills'].render(obj=obj)
-    index.add('b', objects, renderer, substrs=True, storekeys=storekeys)
+    return index
 
+    # spec.update(session='20112012')
+    # storekeys = ['bill_id', 'title', '_type', 'subjects', 'type', 'scraped_subjects',
+    #              'state', '_id', 'session']
+    # objects = db.bills.find(spec)
+    # print 'adding', objects.count(), 'bills', 'with spec %r' % spec
+    # renderer = lambda obj: templates['bills'].render(obj=obj)
+    # index.add('b', objects, renderer, substrs=True, storekeys=storekeys)
+
+if __name__ == '__main__':
+    import sys
+    state = sys.argv[1]
+    index = build_index(state)
     jd = index.jsondata()
     js = index.as_json(showsizes=True)
-    with open('index.json', 'w') as f:
-        index.dump(f)
+    print index.dumps()
