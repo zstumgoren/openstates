d78898d3cd9cea040c4eb84b4b5bf986e9dbb0f2
Tim Showers <showerst@gmail.com>
2016-03-11 22:30:50 -0500

    Stylistic changes to conform to PEP-8

--- openstates/nc/events.py
+++ openstates/nc/events.py
@@ -1,6 +1,6 @@
 import datetime
-import pytz
 import lxml
+import pytz
 
 from billy.scrape.events import EventScraper, Event
 
@@ -8,50 +8,54 @@ from billy.scrape.events import EventScraper, Event
 class NCEventScraper(EventScraper):
     jurisdiction = 'nc'
     _tz = pytz.timezone('US/Eastern')
-    
-    def scrape(self, session, chambers):                
+
+    def scrape(self, session, chambers):
         url = 'http://www.ncleg.net/LegislativeCalendar/'
         html = self.get(url).text
         doc = lxml.html.fromstring(html)
+
+        date_format = '%a, %B %d, %Y %I:%M %p'
+
         for td in doc.xpath('//td[@colspan="3"]'):
-            start_day =td.text_content().strip()
+            start_day = td.text_content().strip()
+
             for tr in td.getparent().itersiblings():
                 #print tr.text_content().replace("\n",'')
                 if tr.xpath('count(.//td)') == 3:
                     tds = tr.xpath('.//td')
-                    
+
                     start_time = tds[0].text_content().strip()
-                    start_time = start_time.replace(u'\xa0',' ').replace(u'\xc2','')
+                    start_time = start_time.replace(u'\xa0', ' ').replace(u'\xc2', '')
                     subject = tds[1].text_content().strip()
                     where = tds[2].text_content().strip()
-                    
+
                     if 'Convenes' not in subject:
-                        when = datetime.datetime.strptime(start_day+' '+start_time, '%a, %B %d, %Y %I:%M %p')
-                        
+                        when = datetime.datetime.strptime(start_day+' '+start_time, date_format)
+
                         meeting_type = 'committee:meeting'
                         com_url = tds[1].xpath('.//a')[0].attrib['href']
                         com_html = self.get(com_url).text
                         com_doc = lxml.html.fromstring(com_html)
                         com_title = com_doc.xpath('//div[@id="title"]')[0].text_content().strip()
                         com_type = com_doc.xpath('//div[@class="titleSub"]')[0].text_content().strip()
-                        
+
                         chambers = {
                             "house" : "lower",
                             "joint" : "joint",
                             "senate" : "upper",
                             "non-standing": "other"
                         }
-                        
+
                         com_chamber = "other"
                         for key in chambers:
                             if key in com_type.lower():
                                 com_chamber = chambers[key]
-                        
+
                         event = Event(session, when, meeting_type,
-                                          subject, where)        
+                                      subject, where)
                         event.add_participant('host', com_title, 'committee',
                                               chamber=com_chamber)
                         event.add_source(url)
-                        self.save_event(event)   
+                        self.save_event(event)
                 else:
                     break
